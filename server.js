cdf8dde0b86b9cde0b15728840368ba9bceee460
Hunter Danielson <hunterleedanielson@gmail.com>
2020-06-15 21:23:25 -0700

    mailgun started

--- server.js
+++ server.js
@@ -3,13 +3,33 @@ require('./lib/utils/connect')();
 
 const app = require('./lib/app');
 const Auction = require('./lib/models/Auction');
+const Bid = require('./lib/models/Bid');
 
 const PORT = process.env.PORT || 7890;
+
+const api_key = 'XXXXXXXXXXXXXXXXXXXXXXX';
+const domain = 'www.mydomain.com';
+const mailgun = require('mailgun-js')({ apiKey: api_key, domain: domain });
+
 
 setInterval(() => {
   Auction.find({ $lte: { end: Date.now() } })
     .then(//accept the highest bids
     );
+
+  // for sending the email with mailgun
+  let emailData;
+  const sendEmail = (creator, title, amount, quantity) => 
+    emailData = {
+      from: 'Buyer@buys.com',
+      to: `${creator}`,
+      subject: `${title}`,
+      text: `Buyer bought ${quantity} of product from your ${title} auction for $${amount}`
+    };
+ 
+  mailgun.messages().send(emailData, function(error, body) {
+    console.log(body);
+  });
 }, 1000 * 60 * 60);
 
 app.listen(PORT, () => {
